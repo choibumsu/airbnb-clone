79eaf552df0d0b7b390b8f605b576288cf3cf4bf
bumsu <bumsu0211@gmail.com>
2020-03-15 20:28:20 +0900

    Feature(user): User admin 페이지 구성

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+# same with @admin.register(models.User) decorator
+# admin.site.register(models.User, CustomUserAdmin)
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+
+    # list_display = ("username", "gender", "language", "currency", "superhost")
+    # list_filter = ("superhost", "currency", "language")
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
